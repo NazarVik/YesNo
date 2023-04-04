8e5b169fd35f9e5c98fac02556e25bf89398ab98
NazarVik <Nazarov_dev@icloud.com>
2023-04-04 22:23:31 +0300

    Initial Commit

---
+++ YesNo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YesNo
+//
+//  Created by Виктор Назаров on 4.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
