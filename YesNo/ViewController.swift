ad4b130229196dd1d426ba46664f3c425101158e
NazarVik <Nazarov_dev@icloud.com>
2023-04-05 15:21:52 +0300

    ready HW1

--- YesNo/ViewController.swift
+++ YesNo/ViewController.swift
@@ -15,7 +15,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        answerButton.layer.cornerRadius = 5
+        answerButton.layer.cornerRadius = 15
+        answerButton.layer.shadowOpacity = 0.9
+        answerButton.layer.shadowOffset = CGSize(width: 1, height: 8)
+        
         // Do any additional setup after loading the view.
     }
 
